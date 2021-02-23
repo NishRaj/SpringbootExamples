4c30786df1709bed3890c3a9875609d722e46616
Nishank Rajvanshi <nishank.rajvanshi@gmail.com>
2021-02-23 23:43:54 +0530

    mapstruct changes

---
+++ springboot-hibernate/src/main/java/com/nishank/HibernateInitializer.java
@@ -0,0 +1,14 @@
+package com.nishank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class HibernateInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateInitializer.class, args);
+        
+    }
+}
